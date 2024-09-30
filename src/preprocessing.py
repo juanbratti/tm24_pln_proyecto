7748d2d8401fec1d6f3c8f602e18c854f147335a
Juan Bratti <juanbratti02@gmail.com>
2024-09-30 00:03:30 -0300

    modularización de funciones y otros detallitos

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils import detect_emojis, get_product_with_max_reviews, tokenize_reviews_to_sequences, get_product_with_n_reviews, lemmatisation_stopwords_series
+from utils import detect_emojis, filter_nouns_spacy, get_product_with_max_reviews, map_lda_to_general_topic, optimal_topic_number, split_into_sentences, tokenize_reviews_to_sequences, get_product_with_n_reviews, lemmatisation_stopwords_series
 import nltk
 import spacy
 
@@ -9,31 +9,19 @@ processed_file_path = '../data/parsed_input_file.csv'
 # turn the csv file to a panda's dataframe
 dataset = pd.read_csv(processed_file_path)
 
-# get the product with ten reviews
+# get the product with 20 reviews
 product_id = get_product_with_n_reviews(processed_file_path,20)
 
-# get the review text for the product 10 reviews
+# get the review text for the product 20 reviews
 reviews = (dataset[dataset['productId'] == product_id])['reviewText']
 
+# detect emojis
+if (detect_emojis(reviews)):
+    print('There are emojis in the reviews')
+else:
+    print('There are no emojis in the reviews')
+
 #######################
-nlp = spacy.load("en_core_web_sm")
-
-def filter_nouns_spacy(text_series):
-    filtered_text = []
-    
-    for review in text_series:
-        # Procesar el texto con spaCy para obtener las categorías gramaticales
-        doc = nlp(review)
-        
-        # Filtrar palabras que no son sustantivos (pos_ == 'NOUN' para sustantivos)
-        non_nouns = [token.text for token in doc if token.pos_ != 'NOUN']
-        
-        # Unir las palabras filtradas en una cadena de texto
-        filtered_text.append(" ".join(non_nouns))
-    
-    return pd.Series(filtered_text)
-
-reviews_without_nouns = filter_nouns_spacy(reviews)
 
 # def filter_adjectives_spacy(text_series):
 #     filtered_text = []
@@ -53,28 +41,26 @@ reviews_without_nouns = filter_nouns_spacy(reviews)
 
 # reviews_without_nouns = filter_adjectives_spacy(reviews)
 
-df_processed = lemmatisation_stopwords_series(reviews_without_nouns)
 
-sequences_list = tokenize_reviews_to_sequences(df_processed, 5)
-#######################
+reviews_sentences = split_into_sentences(reviews)
 
-# detect emojis
-if (detect_emojis(reviews)):
-    print('There are emojis in the reviews')
-else:
-    print('There are no emojis in the reviews')
+processed_series = lemmatisation_stopwords_series(reviews_sentences)
+
+# transform series into list of strings
+sequences_list = processed_series.tolist()
+
+####################### LDA #######################
 
 # vectorization
 from sklearn.feature_extraction.text import CountVectorizer
 
-# vectorizer = CountVectorizer(stop_words="english")
 vectorizer = CountVectorizer()
 dtm = vectorizer.fit_transform(sequences_list)
 
 from sklearn.decomposition import LatentDirichletAllocation
 
 # Apply LDA
-num_topics = 5  # Set the number of topics you want
+num_topics=4
 lda_model = LatentDirichletAllocation(n_components=num_topics, random_state=0)
 lda_model.fit(dtm)
 
@@ -89,32 +75,30 @@ for i, topic in enumerate(lda_model.components_):
     print([vectorizer.get_feature_names_out()[i] for i in topic.argsort()[-10:]])
     print("\n")
 
+# get the topic probability distribution for each review
+with_docs = lda_model.transform(dtm)
+
+# dataframe with the topic probability distribution for each review
+topics_df = pd.DataFrame(with_docs, columns=[f"Topic {i}" for i in range(num_topics)])
+print(topics_df)
 
 ################################################################################################
-def map_lda_to_general_topic(lda_topics, general_topics):
-    mapped_topics = {}
-    for idx, topic_words in enumerate(lda_topics):
-        for general_topic, keywords in general_topics.items():
-            if any(any(keyword in word for word in topic_words) for keyword in keywords):
-                mapped_topics[f"Topic {idx}"] = general_topic
-                break
-        else:
-            mapped_topics[f"Topic {idx}"] = 'Other'
-    return mapped_topics
+
+
 
 # Define general themes and their keywords
 general_topics = {
     "Price": ["cheap", "expensive", "price", "value", "affordable", "cost-effective", "budget-friendly", "inexpensive", "pricy", "overpriced", "costly"],
     "Quality": ["good", "bad", "quality", "durable", "high-quality", "low-quality", "well-made", "fragile", "sturdy", "weak", "reliable", "unreliable"],
     "Use": ["easy", "difficult", "use", "works", "intuitive", "counterintuitive", "straightfoward", "complicated", "efficient", "inefficient", "unreliable"],
-    "Design": ["nice", "ugly", "design", "aesthetic", "stylish", "unstylish", "atractive", "modern", "outdated", "elegant", "tastefull", "tasteless"]
+    "Design": ["nice", "ugly", "design", "aesthetic", "stylish", "unstylish", "atractive", "modern", "outdated", "elegant", "tastefull", "tasteless"],
 }
 
 with_docs = lda_model.transform(dtm)  
 
 lda_topics = [[vectorizer.get_feature_names_out()[i] for i in topic.argsort()[-10:]] for topic in lda_model.components_]
 mapped_topics = map_lda_to_general_topic(lda_topics, general_topics)
-
+print(mapped_topics)
 
 for i in range(len(sequences_list)):
     print(f"Sequence {i + 1}: {sequences_list[i]}")
@@ -123,10 +107,3 @@ for i in range(len(sequences_list)):
     assigned_topic = topic_probability.argmax()
     
     print(f"-- Assigned Topic: {assigned_topic} ({mapped_topics[f'Topic {assigned_topic}']})")
-
-    for j in range(len(mapped_topics)):
-        print(f"-- Topic: {j} ({mapped_topics[f'Topic {j}']}) (Probability: {topic_probability[j]:.2%})")
-    
-    print()
-
-
