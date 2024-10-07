144d853db62c8a9f9fb8be9e6f5d5609886f8e97
ignacio <ignacioscavuzzo14@gmail.com>
2024-10-07 00:21:20 -0300

    bertopic listo casi

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from scripts.preprocessing import load_and_preprocess_data, tokenize_reviews, vectorize_sequences
-from scripts.topic_modelling import apply_bertopic, visualize
+from scripts.topic_modelling import apply_bertopic, visualize, print_model_info
 
 def main():
 
@@ -25,12 +25,12 @@ def main():
         'nouns': False,
         'adj': False,
         'numbers': True,
-        'most_frequent': 3
+        'most_frequent': 0
     }
 
     dataset, reviews_cleaned, product_id = load_and_preprocess_data(params)
 
-    tokens = 0
+    tokens = 5
     # 0 to tokenize in sentences
     # n>0 to tokenize in n-grams
 
@@ -38,27 +38,18 @@ def main():
     sequences_list, sequences_series = tokenize_reviews(reviews_cleaned, tokens) 
     # sequences_list is a python list
     # sequences_series is a pandas series
-
     seed_topic_list = [
         ["price", "cheap", "expensive", "value", "affordable", "cost-effective", "budget-friendly", "inexpensive", "pricy", "overpriced", "costly"],
         ["quality", "good", "bad", "durable", "high-quality", "low-quality", "well-made", "fragile", "sturdy", "weak", "reliable", "unreliable"],
         ["use", "easy", "difficult", "works", "intuitive", "counterintuitive", "straightfoward", "complicated", "efficient", "inefficient", "unreliable"],
         ["design", "nice", "ugly", "aesthetic", "stylish", "unstylish", "attractive", "modern", "outdated", "elegant", "tasteful", "tasteless"]
     ]
-
+    model = "all-MiniLM-L6-v2" # o "umap"
+    reduced_topics = 10
     # application of BERTopic  modeling
-    topic_model = apply_bertopic(sequences_list, seed_topic_list)
+    topic_model = apply_bertopic(sequences_list, seed_topic_list, model, reduced_topics)
 
-     # Display the topics found
-    print("Topics discovered:")
-    print(topic_model.get_topic_info())
-    print(topic_model.get_document_info(sequences_list))
-    print(topic_model.get_representative_docs())
-    print(topic_model.topic_labels_)
-    print(topic_model.hierarchical_topics(sequences_list))
-    print(topic_model.topic_aspects_)
-    
-    print("-----------------------------------------")
+    print_model_info(topic_model, sequences_list)
 
     visualize(topic_model, sequences_list)
 
--- src/scripts/topic_modelling.py
+++ src/scripts/topic_modelling.py
@@ -1,8 +1,9 @@
 from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
 import matplotlib.pyplot as plt
+from umap import UMAP
 
-def apply_bertopic(sequences, seed_topic_list):
+def apply_bertopic(sequences, seed_topic_list, model, reduced_topics):
     """
     Apply BERTopic to extract topics from the reviews using seed topic list.
 
@@ -14,17 +15,23 @@ def apply_bertopic(sequences, seed_topic_list):
         topic_model (BERTopic): Trained BERTopic model.
         topics (list): list of topics found in the sequences.
     """
-
     embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-
     embeddings = embedding_model.encode(sequences, show_progress_bar=True)
-
-    topic_model = BERTopic(seed_topic_list=seed_topic_list)
-    # topic_model = BERTopic()
+    if model == "all-MiniLM-L6-v2":
+        topic_model = BERTopic(seed_topic_list=seed_topic_list)
+        # topic_model = BERTopic()
+    else:
+        umap_model = UMAP(n_neighbors=15,
+                          n_components=5,
+                          min_dist=0.0,
+                          metric="cosine",
+                          random_state=100)
+        topic_model = BERTopic(umap_model=umap_model, language="english", calculate_probabilities=True, seed_topic_list=seed_topic_list)
+        topic_model.fit_transform(sequences, embeddings)
 
     topic_model.fit_transform(sequences, embeddings)
     
-    topic_model.reduce_topics(sequences, nr_topics=10)
+    topic_model.reduce_topics(sequences, nr_topics=reduced_topics)
 
     #Probar el reduce outliers y merge topics
 
@@ -32,18 +39,33 @@ def apply_bertopic(sequences, seed_topic_list):
 
 def visualize(topic_model, sequences_list):
     fig = topic_model.visualize_topics()
-    fig.write_image("./images/topics.png")
+    fig.write_image("./images/bt_topics.png")
     fig = topic_model.visualize_hierarchy()
-    fig.write_image("./images/topic_hierarchy.png")
+    fig.write_image("./images/bt_topic_hierarchy.png")
     fig = topic_model.visualize_barchart()
-    fig.write_image("./images/topic_barchart.png")
+    fig.write_image("./images/bt_topic_barchart.png")
     fig = topic_model.visualize_heatmap()
-    fig.write_image("./images/topic_similarity_heatmap.png")
+    fig.write_image("./images/bt_topic_similarity_heatmap.png")
     fig = topic_model.visualize_term_rank()
-    fig.write_image("./images/term_score_decline.png")
+    fig.write_image("./images/bt_term_score_decline.png")
 
     topic_distr, _ = topic_model.approximate_distribution(sequences_list)
     # Elegir el doc que uno quiera y muestra la distribucion de prob
     fig = topic_model.visualize_distribution(topic_distr[1])
-    fig.write_image("./images/topic_dist.png")
+    fig.write_image("./images/bt_topic_dist.png")
     return
+
+def print_model_info(topic_model, sequences_list):
+    print("Topics discovered:")
+    print(topic_model.get_topic_info())
+    print("-----------------------------------------")
+    print("Document info: ")
+    print(topic_model.get_document_info(sequences_list))
+    # print(topic_model.get_representative_docs())
+    print("-----------------------------------------")
+    #print(topic_model.topic_labels_)
+    print("Heriarchical topics:")
+    print(topic_model.hierarchical_topics(sequences_list))
+    #print(topic_model.topic_aspects_)
+    
+    print("-----------------------------------------")
